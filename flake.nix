3c2eb0a6389b94885708f8be19aed58738c14297
Philipp <philipp@trsk.cc>
2025-12-23 11:28:18 +0100

    setup multi host file hierarchy

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS flake for chonker";
+  description = "NixOS flake for my pcs";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -10,7 +10,7 @@
       nixosConfigurations.chonker = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./hosts/chonker/configuration.nix
           home-manager.nixosModules.home-manager 
           {
             home-manager.useGlobalPkgs = true;
